5677adc3c16ac524cb3e96823772551b046324a5
WebDClassified <vikaschaudhary0720@gmail.com>
2026-02-09 15:24:40 +0530

    new changes

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8000/api",
+  // baseURL: "http://localhost:8000/api",
+  baseURL: "https://careconnect-ai-h20w.onrender.com/api",
 });
